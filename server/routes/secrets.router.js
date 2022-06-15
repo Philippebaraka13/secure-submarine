9666e6d688dcbdb04b498baf64cce681e824cf37
Philippe <kamokamophilippe13@gmail.com>
2022-06-15 14:00:13 -0500

    complete

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
-
-router.get('/', (req, res) => {
+const {
+  rejectUnauthenticated, 
+} = require('../modules/authentication-middleware')
+router.get('/', rejectUnauthenticated, (req, res) => {
   // what is the value of req.user????
   console.log('req.user:', req.user);
 
   pool
-    .query(`SELECT * FROM "secret";`)
+    .query(`SELECT * FROM "secret" WHERE $1 >= secrecy_level;`, [req.user.clearance_level])
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
--- src/index.js
+++ src/index.js
@@ -5,6 +5,21 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 
 import App from './components/App/App';
+import rootReducer from './redux/reducers/_root.reducer';
+import { applyMiddleware } from 'redux';
+import rootSaga from './redux/sagas/_root.saga';
+
+// const sagaMiddleware = createSagaMiddleware();
+// const middlewareList = process.env.NODE_ENV === 'development'?
+// [sagaMiddleware, logger]  :
+// [sagaMiddleware];
+
+// const store = createStore(
+
+//   rootReducer, 
+//   applyMiddleware(...middlewareList), 
+// );
+// sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
